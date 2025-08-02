994fa4a12992f5a513976c39cd8206122af57c34
Vinicius Arraes <viniciusarraes2024@gmail.com>
2025-08-02 15:04:47 -0300

    Movendo o objeto

--- teste.py
+++ teste.py
@@ -4,18 +4,25 @@ from sys import exit
 
 pygame.init()
 
+x = 320 - 25
+y = 0
+
 tela = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('game')
+clock = pygame.time.Clock()
 
 while True:
+    clock.tick(60)
+    tela.fill((0,0,0))
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
 
-    pygame.draw.rect(tela, (0, 255, 0), (320, 240, 40, 50))
-    pygame.draw.circle(tela, (0, 0, 255), (200, 100), 40)
-    pygame.draw.line(tela, (255, 0, 0), (20, 90), (100, 40), 3)
+    pygame.draw.rect(tela, (0, 255, 0), (x, y, 40, 50))
+    if y >= 480:
+        y = 0
+    y += 1
 
     pygame.display.update()
