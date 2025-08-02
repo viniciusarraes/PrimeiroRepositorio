8e85ca7b33e358a8b7aa6b4f6cca68e1de007628
Vinicius Arraes <viniciusarraes2024@gmail.com>
2025-08-02 15:48:43 -0300

    Colisão

--- teste.py
+++ teste.py
@@ -1,11 +1,15 @@
 import pygame
 from pygame.locals import *
 from sys import exit 
+from random import randint
 
 pygame.init()
 
 x = 320
 y = 240
+
+x_ver = randint(20, 235)
+y_ver = randint(20, 230)
 
 tela = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('game')
@@ -25,10 +29,15 @@ while True:
     elif pygame.key.get_pressed()[K_d]:
         x += 5
     elif pygame.key.get_pressed()[K_w]:
-        y -= 5
+        y -= 5 
     elif pygame.key.get_pressed()[K_s]:
         y += 5
+        
+    ret_sla = pygame.draw.rect(tela, (144, 238, 144), (x, y, 40, 40))
+    ret_ver = pygame.draw.rect(tela, (255, 0, 0), (x_ver, y_ver, 40, 40))
 
-    pygame.draw.rect(tela, (144, 238, 144), (x, y, 40, 40))
+    if ret_sla.colliderect(ret_ver):
+        x_ver = randint(20, 235)
+        y_ver = randint(20, 230)
     
     pygame.display.update()
