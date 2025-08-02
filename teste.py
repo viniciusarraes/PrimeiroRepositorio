267f0eb99c4cedbbba1f72f48e6431b01a7540ab
Vinicius Arraes <viniciusarraes2024@gmail.com>
2025-08-02 14:44:15 -0300

    coloquei um retângulo

--- teste.py
+++ teste.py
@@ -5,6 +5,7 @@ from sys import exit
 pygame.init()
 
 tela = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('game')
 
 while True:
 
@@ -12,4 +13,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+    pygame.draw.rect(tela, (200, 10, 34), (320, 240, 40, 50))
+
     pygame.display.update()
