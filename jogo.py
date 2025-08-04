752c76936278f644bc7788e6e1c2e90b56f4200e
Vinicius Arraes <viniciusarraes2024@gmail.com>
2025-08-04 14:04:48 -0300

    começo do snake game

--- jogo.py
+++ jogo.py
@@ -7,26 +7,27 @@ pygame.init()
 
 largura = 640
 altura = 480
-x = largura//2
-y = altura//2
 
-x_azul = randint(40, 60)
-y_azul = randint(50, 430)
+x_cobra = largura//2
+y_cobra = altura//2
+
+x_maca = randint(40, 60)
+y_maca = randint(50, 430)
 
 fonte = pygame.font.SysFont('arial', 40, True, True)
 
 pontos = 0
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('snake_game')
+pygame.display.set_caption('jogo')
 relogio = pygame.time.Clock()
 
 while True:
-    relogio.tick(35)
-    tela.fill((0,0,0))
+    relogio.tick(30)
+    tela.fill((255, 255, 255))
 
     mensagem = f'Pontos: {pontos}'
-    texto_formatado = fonte.render(mensagem, True, (255,255,255))
+    texto_formatado = fonte.render(mensagem, True, (0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -35,20 +36,20 @@ while True:
     
     chave = pygame.key.get_pressed()
     if chave[K_a]:
-        x -= 20
+        x_cobra -= 20
     elif chave[K_d]:
-        x += 20
+        x_cobra += 20
     elif chave[K_w]:
-        y -= 20
+        y_cobra -= 20
     elif chave[K_s]:
-        y += 20
+        y_cobra += 20
     
-    ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x, y, 40, 50))
-    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul, y_azul, 40, 50))
+    cobra = pygame.draw.rect(tela, (0, 255, 0), (x_cobra, y_cobra, 20, 20))
+    maca = pygame.draw.rect(tela, (255, 0, 0), (x_maca, y_maca, 20, 20))
 
-    if ret_vermelho.colliderect(ret_azul):
-        x_azul = randint(40, 600)
-        y_azul = randint(50, 430)
+    if cobra.colliderect(maca):
+        x_maca = randint(40, 600)
+        y_maca = randint(50, 430)
         pontos += 1
 
     tela.blit(texto_formatado, (430, 40))
