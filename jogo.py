659a5f1a475933d8530426a4db27e40ccb78f7af
Vinicius Arraes <viniciusarraes2024@gmail.com>
2025-08-04 15:02:13 -0300

    Jogo da Cobrinha

--- jogo.py
+++ jogo.py
@@ -14,6 +14,10 @@ y_cobra = altura//2
 x_maca = randint(40, 60)
 y_maca = randint(50, 430)
 
+velocidade = 10
+x_controle = 20
+y_controle = 0
+
 fonte = pygame.font.SysFont('arial', 40, True, True)
 
 pontos = 0
@@ -21,6 +25,14 @@ pontos = 0
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('jogo')
 relogio = pygame.time.Clock()
+
+lista_cobra = []
+
+comprimento_inicial = 5
+
+def aumenta_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        pygame.draw.rect(tela, (0, 255, 0), (XeY[0], XeY[1], 20, 20))
 
 while True:
     relogio.tick(30)
@@ -33,16 +45,46 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle = 0
+            if event.key == K_d:
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
+            if event.key == K_w:
+                if y_controle == velocidade:
+                    pass
+                else:
+                    x_controle = 0
+                    y_controle = -velocidade
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = 0
+                    y_controle = velocidade
     
+    x_cobra += x_controle
+    y_cobra += y_controle
+
+    '''
     chave = pygame.key.get_pressed()
     if chave[K_a]:
         x_cobra -= 20
-    elif chave[K_d]:
+    if chave[K_d]:
         x_cobra += 20
-    elif chave[K_w]:
+    if chave[K_w]:
         y_cobra -= 20
-    elif chave[K_s]:
-        y_cobra += 20
+    if chave[K_s]:
+        y_cobra += 20'''
     
     cobra = pygame.draw.rect(tela, (0, 255, 0), (x_cobra, y_cobra, 20, 20))
     maca = pygame.draw.rect(tela, (255, 0, 0), (x_maca, y_maca, 20, 20))
@@ -51,6 +93,18 @@ while True:
         x_maca = randint(40, 600)
         y_maca = randint(50, 430)
         pontos += 1
+        comprimento_inicial += 1
+    
+    lista_cabeca = []
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+
+    lista_cobra.append(lista_cabeca)
+
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
+
+    aumenta_cobra(lista_cobra)
 
     tela.blit(texto_formatado, (430, 40))
     
