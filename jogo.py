235a3cdf20f50ca4d6e811130c9f717fd4fc723c
Vinicius Arraes <viniciusarraes2024@gmail.com>
2025-08-04 12:36:07 -0300

    New Tests - still with problems

--- jogo.py
+++ jogo.py
@@ -1,59 +1,55 @@
 import pygame
 
-#initializing pygame
 pygame.init()
 
-#create a screen (360x480)
-screen = pygame.display.set_mode((400, 480))
+screen_widht = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_widht, screen_height))
 
-#framerate
+pygame.display.set_caption('new_game')
+
+brown = (100, 40, 0)
+blue = (0, 0, 255)
+
+player = pygame.Rect(0, 470, 30, 30)
+platform = pygame.Rect(50, 400, 200, 20)
+ground = pygame.Rect(0, 500, screen_widht, screen_height - 500)
+
 clock = pygame.time.Clock()
 
-#defining a square (player)
-player_x = 0
-player_y = 320 - 30
-
-#ground
-ground = pygame.Rect(0, 320, 400, 480 - 320)
-
-#platform
-
-
-#name the window
-pygame.display.set_caption("main_screen")
-
-#game loop
 running = True
 while running:
     clock.tick(60)
-    #quit event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    pressed_key = pygame.key.get_pressed()
-    if pressed_key[pygame.K_a]:
-        if player_x > 0:
-            player_x -= 3
-    elif pressed_key[pygame.K_d]:
-        if player_x + 30 < 400:
-            player_x += 3
-    elif pressed_key[pygame.K_SPACE]:
-        if player_y + 30 == 320:
-            player_y -= 60
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        player.x -= 5
+    if keys[pygame.K_d]:
+        player.x += 5
+    if keys[pygame.K_SPACE]:
+        player.y -= 5
+
+    if player.colliderect(platform):
+        if player.bottom >= platform.top and player.bottom <= platform.bottom and player.right > platform.left and player.left < platform.right:
+            player.y = platform.top - player.height
+    
+    if player.colliderect(platform):
+        if player.top <= platform.bottom and player.top >= platform.top and player.right > platform.left and player.left < platform.right:
+            player.y = platform.bottom 
+
+    if player.colliderect(ground):
+        player.y += 0
+    else:
+        player.y += 5
     
     screen.fill((0,0,0))
+    pygame.draw.rect(screen, blue, player)
+    pygame.draw.rect(screen, brown, platform)
+    pygame.draw.rect(screen, brown, ground)
 
-    #gravity
-    if player_y + 30 == 320 or (player_y + 30 == 280 and player_x + 30 >= 250 and player_x <= 250 + 80):
-        player_y += 0
-    else:
-        player_y += 1
-
-    pygame.draw.rect(screen, (139, 69, 19), ground)
-    pygame.draw.rect(screen, (128, 0, 128), (player_x, player_y, 30, 30))
-    pygame.draw.rect(screen, (139, 69, 19), (250, 280, 80, 10))
-
-    pygame.display.update()
+    pygame.display.flip()
 
 pygame.quit()
